c36dac581efc644503ad81c13ee115642237c7fa
Morgan Jones <mjones@rice.edu>
2016-10-15 13:22:47 -0500

    force creation of initial migration tables

--- db/migrate/20161015053636_create_projects.rb
+++ db/migrate/20161015053636_create_projects.rb
@@ -1,18 +1,18 @@
 class CreateProjects < ActiveRecord::Migration[5.0]
   enable_extension 'pgcrypto' unless extension_enabled?('pgcrypto')
   def change
-    create_table :projects, id: :uuid, default: 'gen_random_uuid()' do |t|
+    create_table :projects, force: true, id: :uuid, default: 'gen_random_uuid()' do |t|
       t.string :script_name
       t.integer :timeout
     end
 
-    create_table :work_units, id: :uuid, default: 'gen_random_uuid()' do |t|
+    create_table :work_units, force: true, id: :uuid, default: 'gen_random_uuid()' do |t|
       t.references :project, type: :uuid
       t.jsonb :arguments
       t.jsonb :consensus
     end
 
-    create_table :work_requests, id: :uuid, default: 'gen_random_uuid()' do |t|
+    create_table :work_requests, force: true, id: :uuid, default: 'gen_random_uuid()' do |t|
       t.references :work_unit, type: :uuid
       t.datetime :time_issued, default: Time.at(0)
       t.jsonb :result
